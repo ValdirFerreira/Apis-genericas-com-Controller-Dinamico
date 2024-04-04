8a72e39612dc7cb24d94cbbb824737fcc6404a8b
Valdir Ferreira Gomes da Silva <valdir.silva@fcamara.com.br>
2024-04-03 23:31:42 -0300

    Apis-genericas-com-Controller-Dinamico

---
+++ WebApi/Controllers/DynamicController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Config;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DynamicController<T> : ControllerBase where T : class
+    {
+        private readonly AppDbContext _context;
+
+        public DynamicController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<T>>> Get()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<T>> Get(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            return entity;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<T>> Post(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { id = entity.GetType().GetProperty("Id").GetValue(entity) }, entity);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, T entity)
+        {
+            if (id != (int)entity.GetType().GetProperty("Id").GetValue(entity))
+            {
+                return BadRequest();
+            }
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Config;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : DynamicController<Product>
+    {
+        public ProductsController(AppDbContext context) : base(context)
+        {
+        }
+
+
+    }
+}
